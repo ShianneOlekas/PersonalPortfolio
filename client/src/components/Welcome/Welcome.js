816c12d046c60ee6a929fe0ab1dfb4405484f2f7
Ana Olekas <86985160+ShianneOlekas@users.noreply.github.com>
2022-01-14 17:04:42 -0500

    replaced Shianne (Ana) Olekas h1 tag with an SVG for better browser support / trimmed the hello svg to remove excess from the top / finished the welcome section for mobile

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,14 +1,15 @@
 import './Welcome.scss'
+import helloIcon from '../../assets/icons/Hello-cropped.svg'
+import nameLogo from '../../assets/icons/logo.svg'
 
 function Welcome() {
     return (
         <section className='welcome'>
-            {/* this will hold name and small blurb */}
             <div className='welcome__text-container'>
-                <h1 className='welcome__shianne'>Shianne (Ana) Olekas.</h1>
+                <img className='welcome__shianne' src={nameLogo}/>
                 <p className='welcome__paragraph'>Hi. Nice to meet you! I’m a full-stack web dev with a strong interest in CSS and design, in order to bring UI to life. </p>
             </div> 
-            <img className='welcome__vector'/>
+            <img className='welcome__icon' src={helloIcon}/>
         </section>
     );
 }
