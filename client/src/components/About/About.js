fe2d75e10bca5d0942d7487f911f83d02eca8bea
Ana Olekas <86985160+ShianneOlekas@users.noreply.github.com>
2022-01-26 23:02:43 -0500

    created lightning animation / applied animation to about title / currently trying to figure out how to trigger the animation using useRef

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,9 +1,35 @@
 import './About.scss'
+import { useRef, useEffect } from 'react';
 
 function About() {
+    const aboutRef = useRef();
+
+    useEffect(() => {
+
+    });
+
+    const aboutScrollHandler = () => {
+        const currentScroll = window.scrollY;
+        const aboutTitleLocation = aboutRef.current
+
+        if(aboutTitleLocation) {
+            aboutTitleLocation.className.add(`about__letters--trigger-animation`)
+        }
+    }
+
+
     return (
         <section className='about'>
-            
+            <div className="about__title">
+                <ul className="about__list">
+                    <li className='about__letters' ref={aboutRef}>A</li>
+                    <li className='about__letters'>B</li>
+                    <li className='about__letters'>O</li>
+                    <li className='about__letters'>U</li>
+                    <li className='about__letters'>T</li>
+                    <li className='about__letters'>.</li>
+                </ul>
+            </div>
         </section>
     );
 }
